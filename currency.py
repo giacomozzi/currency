f85be4c75d5f7790f84b3a20381a228721e85c4d
giacomozzi <delton.giacomozzi@gmail.com>
2020-05-16 14:59:42 -0300

    Initial commit

--- currency.py
+++ currency.py
@@ -1,20 +1,19 @@
 class Currency(NeuronModule):
-def __init__(self, **kwargs):
-    super(Currency, self).__init__(**kwargs)
-    # the args from the neuron configuration
-    self.currency = kwargs.get('currency', None)
+    def __init__(self, **kwargs):
+        super(Currency, self).__init__(**kwargs)
+        # the args from the neuron configuration
+        self.currency = kwargs.get('currency', None)
 
-    # check if parameters have been provided
-    if self._is_parameters_ok():
+        # check if parameters have been provided
+        if self._is_parameters_ok():
 
-        result = dict()
-        response = requests.get('https://api.hgbrasil.com/finance')
-        result = response.json()
-        result = result['results']['currencies']
-        result = result[self.currency]['buy']
-        self.say(result)
-
-def _is_parameters_ok(self):
+            result = dict()
+            response = requests.get('https://api.hgbrasil.com/finance')
+            result = response.json()
+            result = result['results']['currencies']
+            result = result[self.currency]['buy']
+            self.say(result)
+    def _is_parameters_ok(self):
 
     if self.arg1 is None:
         raise MissingParameterException("You must specify a currency")
